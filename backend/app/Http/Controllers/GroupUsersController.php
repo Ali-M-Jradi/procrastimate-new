7e37012dff5bb71c1f0f23adea80fa1b3dc31e8b
Ali-M-Jradi <jradialimohamad@gmail.com>
2025-05-17 19:00:30 +0300

    setup and migrations

---
+++ backend/app/Http/Controllers/GroupUsersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\group_users;
+use Illuminate\Http\Request;
+
+class GroupUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(group_users $group_users)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(group_users $group_users)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, group_users $group_users)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(group_users $group_users)
+    {
+        //
+    }
+}
